68879f662b467a818b6e94a2ab838720d2a63416
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-11-28 12:28:18 +0100

    Create maven project

---
+++ src/main/java/de/allianz/2811test/2811testApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.2811test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 2811testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(2811testApplication.class, args);
+	}
+
+}
